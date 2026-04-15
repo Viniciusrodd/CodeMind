e9c6145603ba7dcbabf40e790f592a49df400a70
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-04-15 13:55:14 -0300

    security/data middlewares config - server setup

--- backend/src/app/server.ts
+++ backend/src/app/server.ts
@@ -1,6 +1,10 @@
 
 // imports
 import { Application, json, urlencoded } from "express";
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import compression from 'compression';
 
 // import env
 import dotenv from 'dotenv';
@@ -12,6 +16,36 @@ class Server {
    // start server functions
    public async start(app: Application): Promise<void> {      
       this.startServer(app);
+      
+      this.securityMiddlewares(app);
+      this.dataMiddlewaresConfig(app);
+   };
+
+
+   // security middlewares
+   private securityMiddlewares(app: Application): void {
+      app.use(hpp()); // prevents against "HTTP Parameter Pollution"
+      app.use(helmet()); // safety config HTTP headers
+      app.use(cors({
+         origin: process.env.CLIENT_URL,
+         credentials: true,
+         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+      }));
+
+      console.log('✔️ Security middlewares');
+   };
+
+
+   // data middlewares config
+   private dataMiddlewaresConfig(app: Application): void {
+      app.use(compression()); // compresses the data from HTTP responses
+      app.use(json({ limit: '50mb' }));
+      app.use(urlencoded({
+         extended: true,
+         limit: '50mb'
+      }));
+
+      console.log('✔️ Data middlewares');
    };
 
 
