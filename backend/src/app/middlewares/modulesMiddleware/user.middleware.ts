7498b1b8261987a7724167ee862bff84b8affc29
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-04-18 13:32:59 -0300

    middleware - user validations middleware

---
+++ backend/src/app/middlewares/modulesMiddleware/user.middleware.ts
@@ -0,0 +1,33 @@
+
+// imports
+import { body, ValidationChain } from 'express-validator';
+
+
+class UserValidations {
+
+   // user creation
+   public creation(): ValidationChain[] {
+      return [
+         // invalid name
+         body('name')
+            .notEmpty().withMessage('Nome é obrigatório')
+            .isString().withMessage('Nome apenas em caracteres')
+            .isLength({ min: 4, max: 120 }).withMessage('Nome deve ser entre 4 e 120 caracteres')
+            .trim(),
+      ]
+   };
+
+   // user update
+   public update(): ValidationChain[] {
+      return [
+         // invalid name
+         body('name')
+            .notEmpty().withMessage('Nome não pode ser vazio')
+            .isString().withMessage('Nome apenas em caracteres')
+            .isLength({ min: 4, max: 120 }).withMessage('Nome deve ser entre 4 e 120 caracteres')
+            .trim(),
+      ]
+   };
+
+};
+export const userValidations: UserValidations = new UserValidations();
