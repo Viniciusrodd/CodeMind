fbd1a401858035ec7f89768901864da4e542d7f9
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-04-18 13:22:36 -0300

    middleware - handleValidation

---
+++ backend/src/app/middlewares/handleValidation.middleware.ts
@@ -0,0 +1,30 @@
+
+// imports
+import { validationResult } from "express-validator";
+import { Request, Response, NextFunction } from "express";
+
+// import interfaces
+import { iApiResponse } from "@typesGlobal/apiResponse.interface";
+
+
+export const validate = (
+   req: Request,
+   res: Response<iApiResponse>,
+   next: NextFunction
+): Response | void => {
+   // get errors
+   const errors = validationResult(req);
+
+   // without errors:
+   if(errors.isEmpty()) return next();
+
+   // with errors:
+   const extractedErrors: Array<String> = [];
+   errors.array().map((err) => extractedErrors.push(err.msg));
+   
+   return res.status(422).json({
+      success: false,
+      message: '❌ Validations Error',
+      errorMessage: extractedErrors
+   });
+};
