3471b7a9bbad75b4a75d9a879bd99418d975bb34
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-05-11 16:44:16 -0300

    routes - document routes setup

--- backend/src/modules/document/routes/document.routes.ts
+++ backend/src/modules/document/routes/document.routes.ts
@@ -7,6 +7,49 @@ import { documentController } from "@document/controllers/document.controller";
 
 // import middlewares
 import { handleValidation } from "@app/middlewares/handleValidation.middleware";
+import { documentValidations } from "@app/middlewares/modulesMiddleware/document.middleware";
 
 // export router
 export const documentRoutes: Router = Router();
+
+
+//// document routes - 5225
+
+
+// create document
+documentRoutes.post(
+   '/document',
+   documentValidations.creation(),
+   handleValidation,
+   documentController.createDocument
+);
+
+// get all documents
+documentRoutes.get(
+   '/documents',
+   documentController.getAllDocuments
+);
+
+// get document by id
+documentRoutes.get<{ id: string }>(
+   '/document/:id',
+   documentValidations.getDocumentById(),
+   handleValidation,
+   documentController.getDocumentById
+);
+
+// udpate document
+documentRoutes.put<{ id: string }>(
+   '/document/:id',
+   documentValidations.update(),
+   handleValidation,
+   documentController.updateDocument
+);
+
+// delete document
+documentRoutes.delete<{ id: string }>(
+   '/document/:id',
+   documentValidations.delete(),
+   handleValidation,
+   documentController.deleteDocument
+);
