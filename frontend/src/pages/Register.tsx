3e8805294549fbd848ebc80f72e35cea7cdcec72
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-05-21 14:55:16 -0300

    pages - register structure + style

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -8,13 +8,25 @@ import codemind_img from '@images/codemind.png';
 
 const Register = () => {
    return (
-      <div className={ styles.container }>
+      <div className={ styles['page-container'] }>
+         { /* title */ }
          <div className='title-container'>
             <h1 className='codemind-title'>
                CodeMind
             </h1>
             <img className='img-title' src={ codemind_img } alt="codemind.png" />
          </div>
+
+         { /* register */ }
+         <div className={ styles['register-container'] }>
+            <p>Olá, como devemos te chamar ?</p>
+            <input type="text" name="name" title='name' placeholder='Insira seu nome aqui...' />
+         </div>
+
+         { /* send */ }
+         <button type='button' className={ styles['btt-register'] }>
+            ENVIAR
+         </button>
       </div>
    );
 };
