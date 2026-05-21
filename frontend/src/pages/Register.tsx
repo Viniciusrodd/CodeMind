e47533612c3137d82a2f29e91d75f07f88a28b52
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-05-21 14:27:28 -0300

    pages - images added + app title build

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,11 +2,19 @@
 // import css
 import styles from '@styles/pages/Register.module.css';
 
+// import images
+import codemind_img from '@images/codemind.png';
+
 
 const Register = () => {
    return (
       <div className={ styles.container }>
-         <p></p>
+         <div className='title-container'>
+            <h1 className='codemind-title'>
+               CodeMind
+            </h1>
+            <img className='img-title' src={ codemind_img } alt="codemind.png" />
+         </div>
       </div>
    );
 };
