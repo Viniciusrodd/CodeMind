43262d295d113f0c3932e0311b0ac5113218c5b3
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-05-22 13:25:22 -0300

    routes - api routes setup

---
+++ frontend/src/routes/routes.ts
@@ -0,0 +1,12 @@
+
+// user route - GET, PUT, POST, DELETE
+export const userRoute: string = 'http://localhost:5225/api/users';
+
+// project route - GET, PUT, POST, DELETE
+export const projectRoute: string = 'http://localhost:5225/api/projects';
+
+// document route - GET, PUT, POST, DELETE
+export const documentRoute: string = 'http://localhost:5225/api/documents';
+
+// analysis route - GET, POST, DELETE
+export const analysisRoute: string = 'http://localhost:5225/api/analysis';
