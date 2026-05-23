6e55b38cf0d96ccb0055ab13473e2680a5bba128
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-05-23 08:40:57 -0300

    contexts - main loading provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
+
+// imports
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// import providers
+import { LoadingProvider } from '@contexts/loading/loading.provider.tsx'
+
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+   <StrictMode>
+      <LoadingProvider>
+
+         <App />
+      
+      </LoadingProvider>
+   </StrictMode>,
+);
