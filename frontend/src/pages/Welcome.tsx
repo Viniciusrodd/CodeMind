b10235225dea17f0f068f125e5dbdca43a03d7da
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-05-24 08:59:55 -0300

    welcome - structure/style page

--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -1,12 +1,43 @@
 
+// imports
+import { useState } from 'react';
+
 // import css
 import styles from '@styles/pages/Welcome.module.css';
 
+// import components
+import Title from '@components/Title';
+
 
 const Welcome = () => {
+   //// variables
+   const [ name, setName ] = useState<string>('Vini');
+   const [ hasProject, setHasProject ] = useState<boolean>(false);
+
+
+   //// jsx
+
+
    return (
-      <div>
-         <p className={styles.p}>aaa</p>
+      <div className={ styles['page-container'] }>
+         {/* title */}
+         <Title />
+
+         { /* welcome */ }
+         <p className={ styles.welcome }>
+            { name }, seu analisador de códigos já está no ar 🚀
+         </p>
+
+         { /* send */ }
+         { hasProject ? (
+            <button type='button'>
+               VISITAR MEUS PROJETOS
+            </button>
+         ) : (
+            <button type='button'>
+               CRIAR MEU PRIMEIRO PROJETO
+            </button>
+         ) }
       </div>
    );
 };
