18adc6fc1d395936bf43249b551b911829ad071b
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-06-10 09:58:20 -0300

    projectAnalysis - filled fields structure + style

--- frontend/src/pages/projects/ProjectAnalysis.tsx
+++ frontend/src/pages/projects/ProjectAnalysis.tsx
@@ -1,12 +1,30 @@
 
+// imports
+import { useState } from 'react';
+
 // import css
 import styles from '@styles/pages/Projects/ProjectAnalysis.module.css';
+
+// import images
+import check_img from '@images/check.png';
 
 // import components
 import Title from '@components/Title';
 
 
 const ProjectAnalysis = () => {
+   //// variables
+   const [ isCodeFilled, setIsCodeFilled ] = useState<boolean>(false);
+   const [ isContextFilled, setIsContextFilled ] = useState<boolean>(false);
+   const isAllFilled = isCodeFilled && isContextFilled;
+
+
+   //// functions
+
+
+   //// jsx
+
+
    return (
       <div className={ styles['page-container'] }>
          {/* title */}
@@ -16,33 +34,49 @@ const ProjectAnalysis = () => {
          <div className={ styles['inputs-container'] }>
 
             {/* code container */}
-            <div className={ styles['code-container'] }>
-               <div className={ styles.input }>
-                  <h1>Cole o seu código</h1>
+            { isCodeFilled ? (
+               <div className={styles['filled-container'] }>
+                  <div className={ styles.advice }>
+                     <h1>Código inserido</h1>
 
-                  <textarea 
-                     name="" 
-                     id=""
-                     title='code'
-                     placeholder='Código aqui...'
-                     maxLength={ 5000 }
-                  >
-                  </textarea>
-                  <p>*limite de 5 mil caracteres...</p>
-               </div>
+                     <div className={ styles['img-container'] }>
+                        <img src={ check_img } alt="check_img" />
+                     </div>
+                  </div>
 
-               <div className={ styles['buttons-container'] }>
-                  <button>
-                     INSERIR CÓDIGO
-                  </button>
-                  
-                  <p>ou</p>
-                  
-                  <button>
-                     UPLOAD DO ARQUIVO
+                  <button onClick={ () => setIsCodeFilled(false) }>
+                     EDITAR CÓDIGO
                   </button>
                </div>
-            </div>
+            ) : (
+               <div className={ styles['code-container'] }>
+                  <div className={ styles.input }>
+                     <h1>Cole o seu código</h1>
+
+                     <textarea 
+                        name="" 
+                        id=""
+                        title='code'
+                        placeholder='Código aqui...'
+                        maxLength={ 5000 }
+                     >
+                     </textarea>
+                     <p>*limite de 5 mil caracteres...</p>
+                  </div>
+
+                  <div className={ styles['buttons-container'] }>
+                     <button onClick={ () => setIsCodeFilled(true) }>
+                        INSERIR CÓDIGO
+                     </button>
+                     
+                     <p>ou</p>
+                     
+                     <button>
+                        UPLOAD DO ARQUIVO
+                     </button>
+                  </div>
+               </div>
+            ) }
 
             {/* line division */}
             <div className={ styles['line-division'] }>
@@ -50,33 +84,56 @@ const ProjectAnalysis = () => {
             </div>
 
             {/* context container */}
-            <div className={ styles['context-container'] }>
-               <div className={ styles.input }>
-                  <h1>Contexto adicional (opcional)</h1>
+            { isContextFilled ? (
+               <div className={styles['filled-container'] }>
+                  <div className={ styles.advice }>
+                     <h1>Contexto inserido</h1>
 
-                  <textarea 
-                     name="" 
-                     id=""
-                     title='context'
-                     placeholder='Contexto aqui...'
-                     maxLength={ 5000 }
-                  >
-                  </textarea>
-                  <p>*limite de 5 mil caracteres...</p>
+                     <div className={ styles['img-container'] }>
+                        <img src={ check_img } alt="check_img" />
+                     </div>
+                  </div>
+
+                  <button onClick={ () => setIsContextFilled(false) }>
+                     EDITAR CONTEXTO
+                  </button>
                </div>
+            ) : (
+               <div className={ styles['context-container'] }>
+                  <div className={ styles.input }>
+                     <h1>Contexto adicional (opcional)</h1>
 
-               <button>
-                  ADICIONAR CONTEXTO
-               </button>
+                     <textarea 
+                        name="" 
+                        id=""
+                        title='context'
+                        placeholder='Contexto aqui...'
+                        maxLength={ 5000 }
+                     >
+                     </textarea>
+                     <p>*limite de 5 mil caracteres...</p>
+                  </div>
 
-               <div className={styles.examples }>
-                  <h3>Exemplos:</h3>
-                  <p>1. Objetivo do código</p>
-                  <p>2. Dúvidas especificas</p>
-                  <p>3. Foco da análise</p>
+                  <button onClick={ () => setIsContextFilled(true) }>
+                     ADICIONAR CONTEXTO
+                  </button>
+
+                  <div className={styles.examples }>
+                     <h3>Exemplos:</h3>
+                     <p>1. Objetivo do código</p>
+                     <p>2. Dúvidas especificas</p>
+                     <p>3. Foco da análise</p>
+                  </div>
                </div>
-            </div>
+            ) }
          </div>
+
+         {/* code + context filled */}
+         { isAllFilled && (
+            <button>
+               GERAR ANÁLISE
+            </button>
+         ) }
       </div>
    );
 };
