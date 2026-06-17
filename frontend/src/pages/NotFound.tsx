d18c2bd9af08b2dc1ddeb85dc34a0b03ab8c3c34
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-06-17 07:24:54 -0300

    notFound page - structure + style

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,9 +1,28 @@
 
+// imports
+import { useNavigate } from "react-router-dom";
+
+// import css
+import styles from '@styles/pages/NotFound.module.css';
+
 
 const NotFound = () => {
+   //// variables
+   const navigate = useNavigate();
+
+
+   //// jsx
+   
+   
    return (
-      <div>
-         
+      <div className={ styles.container }>
+         <h1>Oops!</h1>
+         <h2>404 - Página não encontrada...</h2>
+         <button type='button' onClick={ () => navigate('/project/dashboard') }>
+            PÁGINA PRINCIPAL
+         </button>
+
+         <p>CodeMind</p>
       </div>
    );
 };
